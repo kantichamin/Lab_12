8a9d52cbfa655e1b1a886ca2689cb3ed40731bea
Kanticha <kantian.0633717109@gmail.com>
2025-12-31 19:06:00 +0700

    fibonacci Lab_12

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int fibonacci(int x);
 
 int main(){
-    clock_t start = clock();
-    cout << "Result: " << fibonacci(50) << "\n";
-    clock_t end = clock();
-    double elapsed = double(end - start)/CLOCKS_PER_SEC;
-    cout << "Elapsed Time: " << elapsed << " seconds.";
-    return 0;
+    int x;
+    cin >> x;
+    cout << fibonacci(x);
+    return 0; 
 }
 
 int fibonacci(int x){
